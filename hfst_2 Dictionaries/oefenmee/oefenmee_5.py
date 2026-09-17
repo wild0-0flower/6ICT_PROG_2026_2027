49b4ad3259e45724b74e2f2755f5aafe6c783b5e
wild0-0flower <creeper4house@gmail.com>
2026-09-17 11:51:45 +0200

    les is gedaan

--- hfst_2 Dictionaries/oefenmee/oefenmee_5.py
+++ hfst_2 Dictionaries/oefenmee/oefenmee_5.py
@@ -4,8 +4,9 @@ scores = [9, 3, 7.5]
 
 filmscores = {}
 for index, film in enumerate(films):
-    sleutel = films[film] # De sleutel is de huidige film.
+    sleutel = films[index] # De sleutel is de huidige film.
     waarde = scores[index]  # De waarde is de overeenkomstige score.
+    filmscores[sleutel] = waarde
     # Gebruik sleutel/waarde om nieuw dict element te maken.
-    filmscores = sleutel
+    
 print(filmscores)
--- hfst_2 Dictionaries/oefenmee/oefenmee_6.py
+++ hfst_2 Dictionaries/oefenmee/oefenmee_6.py
@@ -4,3 +4,5 @@ filmscores = {
     "avatar": 3,
     "oppenheimer": 7.5
 }
+for sleutel, film in filmscores.items():
+    print(f"De film {sleutel} kreeg een score van {filmscores[film]} op 10") #KLOPT NIET
